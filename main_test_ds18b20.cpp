f15bad9241fc5682fad71e7b58df7330687c0f6f
Overlord75780 <jeremydu75@yahoo.fr>
2016-03-18 19:17:57 +0100

    Added and tested glib to get many_sensor in conf

--- main_test_ds18b20.cpp
+++ main_test_ds18b20.cpp
@@ -4,10 +4,26 @@
 #include <glib.h> 
 using namespace std;
 
+const char* config_file = "./conf.txt";
 int main()
 {
+	GKeyFile* gkf;
+	gkf = g_key_file_new();
 	
+	// Loads the config file and tests that everything went OK .
+    if (!g_key_file_load_from_file(gkf, config_file, G_KEY_FILE_NONE, NULL)){
+        fprintf (stderr, "Could not read config file %s\n", config_file);
+        return -1;
+    }	
+	// START stuff about the config file ......
+	int many_sensor = g_key_file_get_integer(gkf, "Sensor", "many_sensor", NULL);
+	cout << many_sensor << endl;
+/*	
 
+	// END stuff about the config file ......
+
+	// we need to free before leaving
+	g_key_file_free(gkf);
 	Ds18b20 *sensor = new Ds18b20("test");
 	float temp; 
 	for (int i=0; i<10; i++)
@@ -16,5 +32,8 @@ int main()
 		cout << temp << endl;
 		sleep(1);
 	}
+	*/
 	return 0;
 }
+// how to use glib
+// https://github.com/joehakimrahme/Agros/wiki/Parsing-the-conf-file-with-GLib
