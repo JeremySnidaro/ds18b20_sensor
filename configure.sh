b314f724522276b630cac1afa12617f0122a9c02
Sni <jeremydu75@yahoo.fr>
2016-03-21 11:14:56 +0100

    added comment on configure.sh

--- configure.sh
+++ configure.sh
@@ -5,8 +5,10 @@ sudo modeprobe w1-therm
 
 # check if the two line needed in /boot/config.txt is present
 # ....
-
+# if the two line is not present write it at the end
 echo \#1-wire setting \n dtoverlay=w1-gpio,gpiopin=4 > /boot/config.txt
+# else do nothing reboot is not needed if the two line is already present
 
 # Reboot is needed to apply the configuration
-sudo reboot
+echo "Reboot is needed to apply the configuration"
+#sudo reboot
