03febcdac5261c445f72ed5bd8a465ba4368dafe
Sni <jeremydu75@yahoo.fr>
2016-03-25 14:11:12 +0100

    add comment and decomment the reboot the script will go in /ect/init.d

--- configure.sh
+++ configure.sh
@@ -9,8 +9,10 @@ echo w1-therm > /etc/modules
 # ....
 # if the two line is not present write it at the end
 echo \#1-wire setting \n dtoverlay=w1-gpio,gpiopin=4 > /boot/config.txt
+#											not sure if gpiopin=4 is needed
 # else do nothing reboot is not needed if the two line is already present
 
 # Reboot is needed to apply the configuration
 echo "Reboot is needed to apply the configuration"
-#sudo reboot
+sleep 5
+reboot
