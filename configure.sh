0d9cf9e5d9354dd054fdd5cb323fc97e75a0e6ae
Sni <jeremydu75@yahoo.fr>
2016-03-27 15:38:26 +0200

    tested and finished , everything is OK

--- configure.sh
+++ configure.sh
@@ -6,11 +6,11 @@
 
 # Care if you use Red Hat/Fedora core/Cent OS 
 # the file need to be /etc/modules.conf
-change=-1
+change=0
 
 if  ! grep -q 'w1-gpio' /etc/modules  &&  ! grep -q 'w1-therm' /etc/modules  ;
 then
-	let "change += 1"
+	let "change+=1"
 	echo w1-gpio >> /etc/modules 
 	echo w1-therm >> /etc/modules 
 fi
@@ -18,13 +18,13 @@ fi
 # check if the two line needed in /boot/config.txt is present
 if  ! grep -q 'dtoverlay=w1-gpio' /boot/config.txt  ;
 then
-	let "change += 1"
+	let "change+=1"
 	# if the line is not present write it at the end
 	echo "#1-wire setting \ndtoverlay=w1-gpio,gpiopin=4" >> /boot/config.txt
-	#											not sure if gpiopin=4 is needed
+											# not sure if gpiopin=4 is needed
 fi
 
-if  $change  ;
+if [ $change -gt 0 ] ;
 then
 	echo "Reboot is needed to apply the configuration, reboot in 5 seconds"
 	sleep 5
