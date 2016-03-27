0b5136c2536975bbfce38e69fb963fb36c1e1537
Sni <jeremydu75@yahoo.fr>
2016-03-27 15:21:56 +0200

    add if statement to see if module are already present is /etc/modules

--- configure.sh
+++ configure.sh
@@ -6,17 +6,27 @@
 
 # Care if you use Red Hat/Fedora core/Cent OS 
 # the file need to be /etc/modules.conf
-echo w1-gpio >> /etc/modules 
-echo w1-therm >> /etc/modules 
+change = -1
+
+if [ ! grep -q 'w1-gpio' /etc/modules ] && [ ! grep -q 'w1-therm' /etc/modules ] ;
+then
+	change++
+	echo w1-gpio >> /etc/modules 
+	echo w1-therm >> /etc/modules 
+fi
 
 # check if the two line needed in /boot/config.txt is present
 if ! grep -q 'dtoverlay=w1-gpio' /boot/config.txt ;
 then
+	change++
 	# if the line is not present write it at the end
 	echo "#1-wire setting \ndtoverlay=w1-gpio,gpiopin=4" >> /boot/config.txt
 	#											not sure if gpiopin=4 is needed
-	echo "Reboot is needed to apply the configuration"
+fi
+
+if [ change ] ;
+then
+	echo "Reboot is needed to apply the configuration, reboot in 5 seconds"
 	sleep 5
 	reboot
 fi
-
