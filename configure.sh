3c90b387fd5b4f74d52343a6ef4f0bebc04a81a2
Sni <jeremydu75@yahoo.fr>
2016-03-27 15:25:36 +0200

    add variable to see if change are made or not and reboot if yes

--- configure.sh
+++ configure.sh
@@ -6,19 +6,19 @@
 
 # Care if you use Red Hat/Fedora core/Cent OS 
 # the file need to be /etc/modules.conf
-change = -1
+change=-1
 
 if [ ! grep -q 'w1-gpio' /etc/modules ] && [ ! grep -q 'w1-therm' /etc/modules ] ;
 then
-	change++
+	let "change += 1"
 	echo w1-gpio >> /etc/modules 
 	echo w1-therm >> /etc/modules 
 fi
 
 # check if the two line needed in /boot/config.txt is present
-if ! grep -q 'dtoverlay=w1-gpio' /boot/config.txt ;
+if [ ! grep -q 'dtoverlay=w1-gpio' /boot/config.txt ] ;
 then
-	change++
+	let "change += 1"
 	# if the line is not present write it at the end
 	echo "#1-wire setting \ndtoverlay=w1-gpio,gpiopin=4" >> /boot/config.txt
 	#											not sure if gpiopin=4 is needed
