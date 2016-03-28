84964f329c6a9fcc9ee29072b441251d89aafc29
Sni <jeremydu75@yahoo.fr>
2016-03-28 23:01:29 +0200

    remote the cout part

--- main_test_app.cpp
+++ main_test_app.cpp
@@ -8,11 +8,5 @@ int main()
 { 
 	configuration config;
 	App *app = new App ("./conf.txt");
-	config = app->load_config();
-	cout << config.port;
-	cout << config.user;
-	cout << config.pwd;
-	cout << config.table;
-	cout << config.time_between_two_value;
 	return 0;
 }
