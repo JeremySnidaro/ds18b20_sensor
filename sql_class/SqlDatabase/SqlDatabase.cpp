34d22ad30a8c0738a222274edfac098061b3ffd5
Overlord75780 <jeremydu75@yahoo.fr>
2016-04-02 17:03:36 +0200

    adapt SqlDatabase for the purpose ( delete useless function)

--- sql_class/SqlDatabase/SqlDatabase.cpp
+++ sql_class/SqlDatabase/SqlDatabase.cpp
@@ -22,11 +22,10 @@ SqlDatabase::SqlDatabase (const char* ipServer,
 
 SqlDatabase::~SqlDatabase()
 {
-  mysql_free_result (result);
   mysql_close (connection);
 }
 
-const char** SqlDatabase::sendQuery (const char* query)
+void SqlDatabase::sendQuery (const char* query)
 {
   //only return the last element and when it's empty
   //it cause a segmentation fault 
@@ -35,38 +34,5 @@ const char** SqlDatabase::sendQuery (const char* query)
   if (state !=0)
   {
     printf (mysql_error (connection));
-  }
-  else
-  {
-    result = mysql_store_result (connection);
-    int num_column = mysql_num_fields (result);
-    const char* resultQuery[num_column];
-    while ((row = mysql_fetch_row (result)))
-      {
-       for(int i = 0; i < num_column; i++)
-    {
-           // Make sure row[i] is valid!
-           if(row[i] != NULL)
-       {
-                resultQuery[i] = row[i];
-       }
-           else
-                resultQuery[i]= "NULL";
-    }
-      }
-    return resultQuery;   
-  }
-}
-bool SqlDatabase::isPresent (const char* query)
-{ 
-  
-  sendQuery (query);
-  if(mysql_num_rows (result))
-  {
-    return true;
-  }
-  else
-  {
-    return false;
   }
 }
--- sql_class/SqlDatabase/SqlDatabase.h
+++ sql_class/SqlDatabase/SqlDatabase.h
@@ -9,15 +9,13 @@ class SqlDatabase
     MYSQL mysql;
     MYSQL_ROW row;
     MYSQL *connection;
-    MYSQL_RES *result;
 
 public:
   SqlDatabase (const char* ipServer, const char* user,
                const char* password, const char* database);
   ~SqlDatabase();
   
-  const char** sendQuery (const char* query);
-  bool isPresent (const char* query);
+  void sendQuery (const char* query);
   
 };
 
