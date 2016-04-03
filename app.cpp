77df2c889a466a4b55e90f7537d6e6c6f115d0a5
Overlord75780 <jeremydu75@yahoo.fr>
2016-04-03 18:28:16 +0200

    rewrite db_check_entry() for test ( set to 1mn)

--- app.cpp
+++ app.cpp
@@ -87,10 +87,10 @@ void App::db_check_entry()
 {
 // delete the entry oldest than 3 years
 // DELETE FROM config.table
-// WHERE config.column_date < UNIX_TIMSTAMP(DATE_SUB(NOW(), INTERVAL (365*3) DAY))
+// WHERE config.column_date < (NOW() - INTERVAL (365*3) DAY)
     string query = "DELETE FROM " + string (config.table)
                  + " WHERE " + string (config.column_date)
-                 + " < UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 1 MINUTE))";
+                 + " < (NOW() - INTERVAL 1 MINUTE)";
 
     db->sendQuery (query.c_str());
 }
